9f74347ea240a24e7fa0edd403d297a61fb6997f
Kritika Kanwar <kritikakanwar27@gmail.com>
2022-07-05 10:13:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ document.querySelectorAll("button")[i].addEventListener("click",handleclick);
 function handleclick()    {
     var buttonValue=this.innerHTML;
     makeSound(buttonValue);
+    buttonAnimation(buttonValue);
     // switch (buttonValue) {
     //     case 'w':
     // var audio=new Audio("sounds/crash.mp3");
@@ -70,8 +71,9 @@ function handleclick()    {
 // On adding keypress event on whole document will include keyboard events also
 document.addEventListener("keypress", function(event){
 console.log(event);
-makeSound(event.key)
-    // alert("Key was pressed");
+makeSound(event.key);
+buttonAnimation(event.key);
+// alert("Key was pressed");
 })  
 
 function makeSound(key)
@@ -80,41 +82,40 @@ function makeSound(key)
         case 'w':
     var audio=new Audio("sounds/crash.mp3");
     audio.play();
-         
             break;
             case 'a':
     var audio=new Audio("sounds/kick-bass.mp3");
     audio.play();
-         
             break;
             case 's':
     var audio=new Audio("sounds/snare.mp3");
     audio.play();
-         
             break;
             case 'd':
     var audio=new Audio("sounds/tom-1.mp3");
     audio.play();
-         
             break;
             case 'j':
     var audio=new Audio("sounds/tom-2.mp3");
     audio.play();
-         
             break;
             case 'k':
     var audio=new Audio("sounds/tom-3.mp3");
     audio.play();
-         
             break;
             case 'l':
     var audio=new Audio("sounds/tom-4.mp3");
     audio.play();
-         
             break;
-            
-    
         default:
             break;
     }
 }
+ function buttonAnimation(currentKey)
+ {
+var activeButton=document.querySelector("."+currentKey).classList.add("pressed");
+setTimeout(function()  
+{
+   var activeButton=document.querySelector("."+currentKey).classList.remove("pressed");
+}, 100);
+ }
